355e0759b78b17298a4475a0695543e18d384bdf
Yuri Petrillo <yuri.petrillo6@gmail.com>
2023-09-15 19:59:38 +0200

    First commerce commit

---
+++ auctions/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=24)
+    description = models.CharField(max_length=64)
+    startingbid = models.IntegerField()
+    imagename = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f"{self.title} ({self.startingprice})"
+    
+    pass
+
+class Bids:
+    def __init__(self, amount):
+        self.amount = amount
+    pass
+
+class Comment:
+    def __init__(self, content):
+        self.content = content
+    pass
